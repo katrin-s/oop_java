a16dca0bcf8c9b460c171a451c1ac4080502d398
katrin.sooaar <katrin.sooaar@khk.ee>
2018-11-15 10:02:32 +0200

    Täiendatud Tervitus tüüpi konstruktorite hulka, mis võimaldab ka tühja sõnega tervitusobjektid luua

--- oop_basics/src/TervitusTest.java
+++ oop_basics/src/TervitusTest.java
@@ -1,6 +1,8 @@
 public class TervitusTest {
     public static void main(String[] args) {
         Tervitus katrinTervitus = new Tervitus("Tere!");
+        Tervitus vaikneTervitus = new Tervitus();
         katrinTervitus.tervita();
+        vaikneTervitus.tervita();
     }
 }
