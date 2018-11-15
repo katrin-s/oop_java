af9cb28d48cd331a011a63382af6dc294633278c
katrin.sooaar <katrin.sooaar@khk.ee>
2018-11-15 11:00:28 +0200

    Loodud Ring tüüpi objekti kirjeldus ja rakendatud Ring tüüpi objekti loomiseks

---
+++ oop_basics/src/Ring.java
@@ -0,0 +1,10 @@
+public class Ring {
+    public double raadius;
+    public double x;
+    public double y;
+
+    public void pindala() {
+        double s = 3.14 * raadius * raadius;
+        System.out.println(s);
+    }
+}
