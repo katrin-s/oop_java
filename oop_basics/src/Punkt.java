5a53038931fdec98d5b22c7190c7a0bb006125a1
katrin.sooaar <katrin.sooaar@khk.ee>
2018-11-22 12:30:40 +0200

    Loodud klass Punkt ja testitud punktide loomist

---
+++ oop_basics/src/Punkt.java
@@ -0,0 +1,26 @@
+public class Punkt {
+    private String nimetus;
+    private Double x;
+    private Double y;
+    private Integer veerand;
+
+    public Punkt(String nimetus, Double x, Double y) {
+        this.nimetus = nimetus;
+        this.x = x;
+        this.y = y;
+        this.setVeerand();
+    }
+
+    public void setVeerand() {
+        if (this.x > 0 & this.y > 0) this.veerand = 1;
+        else if (this.x > 0 & this.y < 0) this.veerand = 2;
+        else if (this.x < 0 & this.y < 0) this.veerand = 3;
+        else if (this.x < 0 & this.y > 0) this.veerand = 4;
+        else this.veerand = 0;
+    }
+
+    public void valjastaKirjeldus() {
+        System.out.println("See on punkt " + this.nimetus + " ja selle koordinaadid on " + this.x + " ja " + this.y);
+        System.out.println("Punkt " + this.nimetus + " asub " + this.veerand + " veerandil");
+    }
+}
