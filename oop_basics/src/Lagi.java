50ca007e3fdcf053a39fa15f9a3e70d90a13f87a
katrin-s <katrin.sooaar@khk.ee>
2018-11-28 22:17:14 +0200

    Lisatud Magamistuba, Sein, Lagi, Voodi, Lamp

---
+++ oop_basics/src/Lagi.java
@@ -0,0 +1,19 @@
+public class Lagi {
+    private Double korgus;
+    private String varv;
+
+    //  konstruktor
+    public Lagi(Double korgus, String varv) {
+        this.korgus = korgus;
+        this.varv = varv;
+    }
+
+    //  getters
+    public Double getKorgus() {
+        return korgus;
+    }
+
+    public String getVarv() {
+        return varv;
+    }
+}
