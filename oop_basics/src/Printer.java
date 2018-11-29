8f5f468f8512628c08902326ada4cca1c36cceaf
katrin.sooaar <katrin.sooaar@khk.ee>
2018-11-29 09:58:05 +0200

    lisatud getterid

--- oop_basics/src/Printer.java
+++ oop_basics/src/Printer.java
@@ -1,11 +1,27 @@
 public class Printer {
-    public Integer tooneriTase;
-    public Integer paberPrintimiseks;
-    public Boolean kahepoolne;
+    private Integer tooneriTase;
+    private Integer paberPrintimiseks;
+    private Boolean kahepoolne;
 
     public Printer(Integer tooneriTase, Boolean kahepoolne) {
         this.tooneriTase = tooneriTase;
         this.kahepoolne = kahepoolne;
         this.paberPrintimiseks = 100;
+    }
+
+    public Integer getTooneriTase() {
+        return tooneriTase;
+    }
+
+    public Integer lisaTooner(Integer toonerJuurde) {
+        if (this.tooneriTase >= 0 & this.tooneriTase <= 100) {
+            if (this.tooneriTase + toonerJuurde > 100) {
+                return -1;
+            }
+            this.tooneriTase = this.tooneriTase + toonerJuurde;
+            return this.tooneriTase;
+        } else {
+            return -1;
+        }
     }
 }
--- oop_basics/src/printerTest.java
+++ oop_basics/src/printerTest.java
@@ -1,11 +1,9 @@
-public class printerTest {
+public class PrinterTest {
     public static void main(String[] args) {
         Printer printer1 = new Printer(50, false);
-        System.out.println("Printeris on " + printer1.paberPrintimiseks + " lehte printimiseks.");
-        System.out.println("Printeri tooneri tsse on " + printer1.tooneriTase);
-        printer1.paberPrintimiseks = 45;
-        printer1.tooneriTase = 120;
-        System.out.println("Printeris on " + printer1.paberPrintimiseks + " lehte printimiseks.");
-        System.out.println("Printeri tooneri tsse on " + printer1.tooneriTase);
+        printer1.lisaTooner(15);
+        System.out.println("Printeri tooneri tase on " + printer1.getTooneriTase() + "%");
+        printer1.lisaTooner(50);
+        System.out.println("Printeri tooneri tase on " + printer1.getTooneriTase() + "%");
     }
 }
