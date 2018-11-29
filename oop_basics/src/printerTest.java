f4fca51960f5abbcf63ccca1e6504d784ab1581a
katrin.sooaar <katrin.sooaar@khk.ee>
2018-11-29 09:38:43 +0200

    Täiendatud printeri klass

--- oop_basics/src/printerTest.java
+++ oop_basics/src/printerTest.java
@@ -1,7 +1,11 @@
 public class printerTest {
     public static void main(String[] args) {
         Printer printer1 = new Printer(50, false);
-        printer1.paberPrintimiseks = 45;
         System.out.println("Printeris on " + printer1.paberPrintimiseks + " lehte printimiseks.");
+        System.out.println("Printeri tooneri tsse on " + printer1.tooneriTase);
+        printer1.paberPrintimiseks = 45;
+        printer1.tooneriTase = 120;
+        System.out.println("Printeris on " + printer1.paberPrintimiseks + " lehte printimiseks.");
+        System.out.println("Printeri tooneri tsse on " + printer1.tooneriTase);
     }
 }
