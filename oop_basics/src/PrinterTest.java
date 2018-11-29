b9003ebded560058df21eb50a25a681bce6134e5
katrin.sooaar <katrin.sooaar@khk.ee>
2018-11-29 11:09:46 +0200

    Muudetud tekst

--- oop_basics/src/PrinterTest.java
+++ oop_basics/src/PrinterTest.java
@@ -1,12 +1,12 @@
 public class PrinterTest {
     public static void main(String[] args) {
-        Printer printer1 = new Printer(150, true);
+        Printer printer1 = new Printer(150, false);
         printer1.lisaTooner(15);
         System.out.println("Printeri tooneri tase on " + printer1.getTooneriTase() + "%");
         Integer kontrolltoo = printer1.prindi(5);
-        System.out.println("Printisin kontrolltöö jaoks " + kontrolltoo + " paberi lehte ja printerisse on jäänud " + printer1.getPaberPrintimiseks() + " paberi lehte");
+        System.out.println("Printisin kontrolltöö jaoks " + kontrolltoo + " paberi lehte ja printerisse on jäänud " + printer1.getPaberPrintimiseks() + " paberilehte");
         printer1.lisaPaber(50);
         kontrolltoo = printer1.prindi(5);
-        System.out.println("Printisin kontrolltöö jaoks " + kontrolltoo + " paberi lehte ja printerisse on jäänud " + printer1.getPaberPrintimiseks() + " paberi lehte");
+        System.out.println("Printisin kontrolltöö jaoks " + kontrolltoo + " paberi lehte ja printerisse on jäänud " + printer1.getPaberPrintimiseks() + " paberilehte");
     }
 }
